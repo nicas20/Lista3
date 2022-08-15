3d4c8921d1d301230dd79755ade86d36ca3882f7
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-15 16:59:07 -0300

    ex9

---
+++ ex9.java
@@ -0,0 +1,14 @@
+package com.mycompany.lista3;
+
+public class ex9 {
+
+    public static void main(String[] args) {
+        System.out.println("Valores ímpares:");
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
