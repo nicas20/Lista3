7bfc0b461300b9f5251185c49f3f644c5bc181e0
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-15 17:22:03 -0300

    ex6a

---
+++ ex6a.java
@@ -0,0 +1,12 @@
+package com.mycompany.lista3;
+
+public class ex6a {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
