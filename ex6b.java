63400383ce33498cc50c80f76009dc3c45f91b41
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-15 17:22:44 -0300

    ex6b

---
+++ ex6b.java
@@ -0,0 +1,12 @@
+package com.mycompany.lista3;
+
+public class ex6b {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+
+        }
+    }
+}
